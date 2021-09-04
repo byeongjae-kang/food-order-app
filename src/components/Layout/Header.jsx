a49d2444b1cdf4efddfe9793e672ee5720fae96c
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-04 14:16:20 -0700

    prop drill to open cart modal

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,12 +3,12 @@ import classes from "./Header.module.css";
 import mealsImg from '../../assets/meals.jpeg'
 import HeaderCartButton from "./HeaderCartButton";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1 className={classes}>Yummi2</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImg} alt="foodImg" className={classes} />
