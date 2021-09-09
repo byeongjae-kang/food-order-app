aaf5e79eef799561b450674597b2941ef5946dea
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-08 23:32:12 -0700

    implement loading and error message feature

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,36 +1,61 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99
-  }
-];
-
 export default function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://yummi2-default-rtdb.firebaseio.com/meals.json`
+      );
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const loadedData = [];
+
+      for (const key in responseData) {
+        loadedData.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        });
+      }
+      setMeals(loadedData);
+      setIsLoading(false);
+    };
+
+    fetchData().catch((err) => {
+      console.log(err.message);
+      setIsLoading(false);
+      setErrorMessage(err.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealIsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (errorMessage) {
+    return (
+      <section className={classes.errorMessage}>
+        <p>{errorMessage}...</p>
+      </section>
+    );
+  }
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
